e84b86880af3d7dc8dfa7a22521b9922a3de0fdb
tysker <joergoertel@proton.me>
2026-09-25 17:36:43 +0200

    create basic multi maven module project for the notify-app

---
+++ notifyhub-app/src/main/java/io/github/tysker/notifyhubapp/NotifyhubAppApplication.java
@@ -0,0 +1,13 @@
+package io.github.tysker.notifyhubapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotifyhubAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotifyhubAppApplication.class, args);
+    }
+
+}
