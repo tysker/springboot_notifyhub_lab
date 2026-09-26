c08ba30d2ab750efa360b0dbe8a7c9d3ebb41c7d
tysker <joergoertel@proton.me>
2026-09-26 13:53:59 +0200

    add lombok to core classpath

--- notifyhub-core/src/main/java/io/github/tysker/notifyhubcore/NotificationService.java
+++ notifyhub-core/src/main/java/io/github/tysker/notifyhubcore/NotificationService.java
@@ -1,13 +1,15 @@
 package io.github.tysker.notifyhubcore;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+@Slf4j(topic = "notifyhub")
 @Service
 public class NotificationService {
 
     @Autowired
     public NotificationService() {
-        System.out.println("NotificationController initialized");
+        log.info("init() --> NotificationService initialized");
     }
 }
